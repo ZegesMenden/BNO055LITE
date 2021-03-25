4e027d6d84e6ed09278ca1501a9283d08b1442af
ZegesMenden <51548005+ZegesMenden@users.noreply.github.com>
2021-03-24 22:11:17 -0400

    Create example_simple.ino

---
+++ examples/example_simple.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+#include "BNOLITE.h"
+
+BNOLITE bno = BNOLITE();
+
+// code written by ZegesMenden
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial); // wait until serial is ready
+
+  Serial.println("BNO055 test!");
+  
+  if (! bno.initialize()) {
+    Serial.println("BNO055 not found!");
+    while(1); // wait forever
+  }
+  
+  Serial.println("BNO055 initialized!");
+}
+
+void loop() {
+  bno.read_gyro(); // read gyro data
+  bno.read_accel(); // read accel data
+
+  Serial.print("gyroscope - X: "); // printing the gyro data
+  Serial.print(bno.gyro.x);
+  Serial.print(" Y: ");
+  Serial.print(bno.gyro.y);
+  Serial.print(" Z: ");
+  Serial.println(bno.gyro.z);
+    
+  Serial.print("accelerometer - X: "); // printing the accel data
+  Serial.print(bno.accel.x);
+  Serial.print(" Y: ");
+  Serial.print(bno.accel.y);
+  Serial.print(" Z: ");
+  Serial.println(bno.accel.z);
+  
+  delay(25);
+}
