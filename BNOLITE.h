26ae2802535a74796206e2b947494e92f32a891e
ZegesMenden <51548005+ZegesMenden@users.noreply.github.com>
2021-05-25 05:38:46 -0700

    Update BNOLITE.h
    removed debias from header

--- BNOLITE.h
+++ BNOLITE.h
@@ -12,12 +12,6 @@ class BNOLITE
 
     bool initialize(); // sensor startup and various other functions - you need to run this line of code for the sensor to work
 
-    void debias(int sample_count); // optional, but recommended - debiases sensor noise and automatically subtracts the noise from future gyro readings.
-    void disable_debias(); // if you accidentally moved the gyroscope while debiasing, or just dont like the debiasing values, you can run this line to disable the debiasing feature.
-    void enable_debias(); // to re-enable the debiasing feature if it was manually disabled. 
-
-    // keep in mind if you run the debias command and have run disable_debias prior, the library will automatically resume debiasing the gyroscope
-
     void read_gyro(); // run this to read gyroscope data. gyroscopic data will be accessible in bno.gyro. x / y / z
     void read_accel(); // run this to read accelerometer data. accelerometer data will be accessible in bno.accel. x / y / z
     void read_mag(); // run this to read magnetometer data. magnetometer data will be accessible in bno.mag. x / y / z
